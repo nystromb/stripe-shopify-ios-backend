dfb41befcc7c314b6bd936d3ee4eace46ad35bf9
nystromb <bnystrom@grubhub.com>
2023-06-03 16:46:31 -0500

    Adds customer key endpoint

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,24 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 /*
-  Explain what this endpoint is supposed to do
+  request should send customer access token
+  use access token to get customer information from shopify, if it exists
+  once we have the information from shopify, then create a stripe customer if it doesn to exist
+  return the payment intent with the proper information
 */
 router.post('/create_payment_intent', function(req, res, next) {
-  // request should send customer access token
-  // use access token to get customer information from shopify, if it exists
-  // once we have the information from shopify, then create a stripe customer if it doesn to exist
-  // return the payment intent with the proper information
-  console.log('request', req);
+  console.log('request', req.query);
   res.send('respond with a resource');
 });
 
-
 /*
   Explain what this endpoint is supposed to do
 */
-router.post('/ephemeral_keys', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/ephemeral_keys', async function(req, res, next) {
+  const { api_version } = req.query;
+  console.log("query", req.query);
+  try {
+    let key = await stripe.ephemeralKeys.create(
+      {customer: 'cus_NYdnkbtwWXbhmQ'
+    },
+      {apiVersion: `${api_version}`}
+    );
+    res.json(key);
+  } catch(e) {
+    console.error(e);
+    res.send(500);
+  }
 });
 
 module.exports = router;
